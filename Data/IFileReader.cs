dd503717d6b320cb511aca56fecc5e3e867379ca
Richard <richard.hriech@outlook.com>
2020-09-15 00:24:51 +0200

    initial commit

---
+++ Data/IFileReader.cs
@@ -0,0 +1,7 @@
+﻿namespace Data
+{
+    public interface IFileReader
+    {
+        
+    }
+}
---
+++ EscapeMines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EscapeMines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
