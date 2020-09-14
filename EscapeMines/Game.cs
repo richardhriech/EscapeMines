76a24e5964f91192fc0358937f18ff72d320607c
Richard <richard.hriech@outlook.com>
2020-09-15 00:48:00 +0200

    Game is singleton

---
+++ EscapeMines/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace EscapeMines
+{
+    public sealed class Game
+    {
+        static Game()
+        {
+        }
+
+        private Game()
+        {
+        }
+
+        public static Game Instance { get; } = new Game();
+    }
+}
